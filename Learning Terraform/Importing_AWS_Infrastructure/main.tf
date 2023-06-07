cb5e60fc062ea5ecce702a2385f1f3f0e7922948
jpauline <jpauline254@gmail.com>
2023-06-07 07:44:31 +0300

    An empty aws_vpc resource block is created as a place holder for the existing VPC in AWS

--- Learning Terraform/Importing_AWS_Infrastructure/main.tf
+++ Learning Terraform/Importing_AWS_Infrastructure/main.tf
@@ -9,4 +9,8 @@ terraform {
 provider "aws" {
   region = "us-west-2"
 }
+
+
+#An empty aws_vpc resource block is created as a place holder for the existing VPC in AWS.
+# Once the resource is imported into a state file, this resource will need to be modified with the proper arguments:
 resource "aws_vpc" "dev" {}
